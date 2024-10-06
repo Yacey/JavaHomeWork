ae26bc4205688c74384fccddc2b74b1e77b2e287
JasonWarner <9655826+jasonwarner@user.noreply.gitee.com>
2024-10-06 17:18:16 +0800

    L02_作业提交

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello Java!\tchen xia 242821806");
+        System.out.printf("Hello Java! chen xia 242821806");
         }
     }
